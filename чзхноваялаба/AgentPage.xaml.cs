090ab4dad8b716646c89d2acf0f8362db8d1a0ec
Timaty <ibigsin52@gmail.com>
2023-10-25 14:09:20 +0500

    Добавьте файлы проекта.

---
+++ чзхноваялаба/AgentPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace чзхноваялаба
+{
+    /// <summary>
+    /// Логика взаимодействия для AgentPage.xaml
+    /// </summary>
+    public partial class AgentPage : Page
+    {
+        public AgentPage()
+        {
+            InitializeComponent();
+            var currentServices = ivanov_GlazkiEntities.GetContext().Agent.ToList();
+
+            ServiceListView.ItemsSource = currentServices;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            manager.MainFrame.GoBack();
+        }
+    }
+}
